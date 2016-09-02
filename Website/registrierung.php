838590e373960b3a99db086d1afbb57db43b0fa0
Markus Hiller <hiller_markus@hotmail.com>
2016-09-02 10:11:02 +0200

    *fixed phpmailer path

--- Website/registrierung.php
+++ Website/registrierung.php
@@ -12,7 +12,7 @@ function email_senden($email, $username, $password) {
 	$betreff = CONFIG_SITE_NAME.': Willkommen';
 	$nachricht = "Hallo ".$username.",\n\nDu hast Dich erfolgreich auf ".CONFIG_SITE_DOMAIN." registriert. Bitte logge Dich jetzt mit Deinen Benutzerdaten ein, um Deinen Account zu aktivieren. Und dann kann es auch schon losgehen ...\n\nDamit Du Dich anmelden kannst, findest Du hier noch einmal Deine Benutzerdaten:\n\nE-Mail: ".$email."\nBenutzername: ".$username."\nPasswort: ".$password."\n\nWir wünschen Dir noch viel Spaß beim Managen!\n\nSportliche Grüße\n".CONFIG_SITE_NAME."\n".CONFIG_SITE_DOMAIN."\n\n------------------------------\n\nDu erhältst diese E-Mail, weil Du Dich auf ".CONFIG_SITE_DOMAIN." mit dieser Adresse registriert hast. Du kannst Deinen Account jederzeit löschen, nachdem Du Dich eingeloggt hast, sodass Du anschließend keine E-Mails mehr von uns bekommst. Bei Missbrauch Deiner E-Mail-Adresse meldest Du Dich bitte per E-Mail unter ".CONFIG_SITE_EMAIL;
 	if (CONFIG_EMAIL_PHP_MAILER) {
-		require './phpmailer/PHPMailerAutoload.php';
+		require_once 'phpmailer/PHPMailerAutoload.php';
 		$mail = new PHPMailer(); // create a new object
 		$mail->CharSet = CONFIG_EMAIL_CHARSET;
 		$mail->IsSMTP();
