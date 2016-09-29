22764af7a31192d928db6539396999f46958ac81
Markus Hiller <hiller_markus@hotmail.com>
2016-09-29 20:13:40 +0200

    *jetzt werde ich nachlässig ;(

--- Website/controller/supportController.php
+++ Website/controller/supportController.php
@@ -8,7 +8,7 @@ class SupportController {
         $supportSql = $status == 'Helfer' || $status == 'Admin' ? "" : " AND visibilityLevel = 0";
         $sql = "SELECT COUNT(*) FROM " . CONFIG_TABLE_PREFIX . "supportRequests WHERE open = 1 AND '" . $userIds . "' NOT IN (SELECT userId FROM " . CONFIG_TABLE_PREFIX . "supportRead WHERE anfrageID = id)" . $supportSql;
         $result = DB::query($sql, FALSE);
-        echo $sql;
+        
         return mysql_result($result, 0);
     }
 
