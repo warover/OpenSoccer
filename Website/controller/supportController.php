7860a2339de71d803a4a17dd86f095dd2723b92d
Markus Hiller <hiller_markus@hotmail.com>
2016-09-29 20:05:08 +0200

    *fixed case-sensitive tablenames for linux

--- Website/controller/supportController.php
+++ Website/controller/supportController.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once(dirname(__FILE__).'/../utils/database.php');
+require_once(dirname(__FILE__) . '/../utils/database.php');
 
 class SupportController {
 
     public static function getUnreadedTicketCount($userIds, $status) {
         $supportSql = $status == 'Helfer' || $status == 'Admin' ? "" : " AND visibilityLevel = 0";
-        $sql = "SELECT COUNT(*) FROM man_supportrequests WHERE open = 1 AND '" . $userIds . "' NOT IN (SELECT userId FROM man_supportread WHERE anfrageID = id)" . $supportSql;
+        $sql = "SELECT COUNT(*) FROM " . CONFIG_TABLE_PREFIX . "supportRequests WHERE open = 1 AND '" . $userIds . "' NOT IN (SELECT userId FROM " . CONFIG_TABLE_PREFIX . "supportRead WHERE anfrageID = id)" . $supportSql;
         $result = DB::query($sql, FALSE);
-        
+        echo $sql;
         return mysql_result($result, 0);
     }
 
